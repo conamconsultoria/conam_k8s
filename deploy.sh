f9253d485a570dd5bbfe58f223662948de8998fb
Leandro Soares <leandro.soares@conam.com.br>
2022-11-30 16:18:22 -0300

    configuracao final

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t leandroconam/conam-app:latest -t leandroconam/conam-app:$SHA -f ./app/Dockerfile ./app
+docker build -t leandroconam/conam-logger:latest -t leandroconam/conam-logger:$SHA -f ./logger/Dockerfile ./logger
+docker build -t leandroconam/conam-portal:latest -t leandroconam/conam-portal:$SHA -f ./portal/Dockerfile ./portal
+
+docker push leandroconam/conam-app
+docker push leandroconam/conam-logger
+docker push leandroconam/conam-portal
+
+kubectl apply -f k8s
+
+kubectl set image deployments/app-deployment conam-app=leandroconam/conam-app:$SHA
+kubectl set image deployments/logger-deployment conam-logger=leandroconam/conam-logger:$SHA
+kubectl set image deployments/portal-deployment conam-portal=leandroconam/conam-portal:$SHA
